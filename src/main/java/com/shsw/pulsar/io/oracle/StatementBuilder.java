1513f3a68d63f8264099550bbd7240e284be3a7f
shsw <sae.sahatsawat@gmail.com>
2024-10-14 01:42:19 +0700

    Add getTableMetaData method: Checking metadata from server and provided data

--- src/main/java/com/shsw/pulsar/io/oracle/StatementBuilder.java
+++ src/main/java/com/shsw/pulsar/io/oracle/StatementBuilder.java
@@ -3,22 +3,26 @@ package com.shsw.pulsar.io.oracle;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Objects;
 
 @Slf4j
 public class StatementBuilder {
 
     @Data(staticConstructor = "of")
     public static class TableMetaData {
-        private String schemaName;
-        private String tableName;
+        private final String schemaName;
+        private final String tableName;
     }
 
     @Data(staticConstructor = "of")
     public static class ColumnMetaData {
-        private String columnName;
-        private String dataType;
-        private Integer position;
+        private final String columnName;
+        private final String dataType;
+        private final Integer position;
     }
 
 
@@ -61,5 +65,29 @@ public class StatementBuilder {
 
     }
 
+
+    // Objective: Query metadata and check that is match with provided data.
+    public static TableMetaData getTableMetaData(Connection connection, String schema, String table) throws Exception {
+        // Query metadata with given schema and table name.
+        try(ResultSet resultSet = connection.getMetaData().getTables(null,
+                schema,
+                table,
+                new String[]{"TABLE", "PARTITIONED TABLE"})) {
+            if(resultSet.next()) {
+                String querySchemaName = resultSet.getString("TABLE_SCHEM");
+                String queryTableName = resultSet.getString("TABLE_NAME");
+
+                // If the queried data and given is mismatch. -> raise an error
+                if (!Objects.equals(queryTableName, table) || !Objects.equals(querySchemaName, schema)) {
+                    throw new IllegalArgumentException("Given schema or table name is mismatch with query results.");
+                }
+
+                return TableMetaData.of(querySchemaName, queryTableName);
+            } else {
+                throw new Exception("Not able to find table: " + table);
+            }
+
+        }
+    }
 
 }
