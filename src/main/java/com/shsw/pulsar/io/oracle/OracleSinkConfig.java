3b6bb3b34fe1f92d25a4d90e71a1577a34d2cee0
shsw <sae.sahatsawat@gmail.com>
2024-10-22 02:07:34 +0700

    Add timeout for flush scheduler

--- src/main/java/com/shsw/pulsar/io/oracle/OracleSinkConfig.java
+++ src/main/java/com/shsw/pulsar/io/oracle/OracleSinkConfig.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
-import java.util.List;
 import java.util.Map;
 
 import lombok.Data;
@@ -54,7 +53,6 @@ public class OracleSinkConfig implements Serializable {
     private String table;
 
     @FieldDoc(
-            required = false,
             defaultValue = "",
             help = "A set of PK name which define as list. for example: [PK1,PK2]"
     )
@@ -68,14 +66,6 @@ public class OracleSinkConfig implements Serializable {
     private String nonKeyColumns;
 
     @FieldDoc(
-            required = true,
-            defaultValue = "",
-            help = "Set of exclude columns"
-    )
-    private String excludeColumns;
-
-    @FieldDoc(
-            required = false,
             defaultValue = "200",
             help = "Enable batch mode by number of operations. This value is the max number of operations "
                     + "batched in the same transaction/batch."
@@ -83,36 +73,35 @@ public class OracleSinkConfig implements Serializable {
     private int batchSize = 200;
 
     @FieldDoc(
-            required = false,
             defaultValue = "false",
             help = "Use JDBC batch API, increase write performance."
     )
     private boolean useJDBCBatch = false;
 
     @FieldDoc(
-            required = false,
             defaultValue = "true",
             help = "Enable transaction of database."
     )
     private boolean useTransaction = true;
 
     @FieldDoc(
-            required = false,
             defaultValue = "INSERT",
-            help = "The insert mode: INSERT, UPSERT, UPDATE."
+            help = "The insert mode: INSERT, UPDATE."
     )
     private InsertMode insertMode = InsertMode.INSERT;
 
-
-
-
+    @FieldDoc(
+            defaultValue = "",
+            help = "timeout in ms"
+    )
+    private Integer timeoutMS = 5000;
 
 
     public enum InsertMode {
         INSERT,
-        UPSERT,
         UPDATE
     }
+
     public static OracleSinkConfig load(String yamlFile) throws IOException {
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
         return mapper.readValue(new File(yamlFile), OracleSinkConfig.class);
