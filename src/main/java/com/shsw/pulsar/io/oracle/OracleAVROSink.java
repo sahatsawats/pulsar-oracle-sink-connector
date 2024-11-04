311152e848cfe0b655b5644c905525913631e06b
shsw <sae.sahatsawat@gmail.com>
2024-11-05 02:18:18 +0700

    [wip] add helper functions and logical-types

--- src/main/java/com/shsw/pulsar/io/oracle/OracleAVROSink.java
+++ src/main/java/com/shsw/pulsar/io/oracle/OracleAVROSink.java
@@ -3,28 +3,22 @@ package com.shsw.pulsar.io.oracle;
 import com.google.protobuf.ByteString;
 import com.shsw.pulsar.io.oracle.core.OracleAbstractSink;
 import com.shsw.pulsar.io.oracle.core.StatementBuilder.ColumnMetaData;
-import oracle.jdbc.proxy.annotation.Pre;
-import oracle.sql.DATE;
 import org.apache.avro.Schema;
 import org.apache.pulsar.client.api.schema.GenericObject;
 import org.apache.pulsar.functions.api.Record;
-import org.apache.pulsar.common.schema.SchemaType;
 import org.apache.avro.generic.GenericRecord;
-
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.sql.Date;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Types;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.time.LocalDate;
+import java.util.*;
 
 public class OracleAVROSink extends OracleAbstractSink<GenericObject> {
 
     @Override
     public void bindValue(PreparedStatement preparedStatement, Record<GenericObject> record) throws Exception {
-
+        // Deserialize record to hashmap field and value.
         Map<String, Object> deserializedAVRO = deserializeRecord(record);
 
         final List<ColumnMetaData> columns = new ArrayList<>();
@@ -41,22 +35,39 @@ public class OracleAVROSink extends OracleAbstractSink<GenericObject> {
 
         for (ColumnMetaData column : columns) {
             Object value = deserializedAVRO.get(column.getColumnName());
+            // Set the statement based on the type of value
             setStatement(preparedStatement, index, value);
             index++;
         }
 
     }
 
+    // TODO: Make a object to holding the record f
     private Map<String, Object> deserializeRecord(Record<GenericObject> record) {
         Map<String, Object> deserializedAVRO = new HashMap<>();
 
         GenericRecord avroNode = (GenericRecord) record.getValue().getNativeObject();
         for (Schema.Field field : avroNode.getSchema().getFields()) {
-            deserializedAVRO.put(field.name(), avroNode.get(field.toString()));
+            Object value = switch (field.getProp("logicalType")) {
+                // TODO: Add timestamp
+                case "date" -> convertToSQLDate((int) avroNode.get(field.toString()));
+                case "decimal" -> convertToBigDecimal((byte[]) avroNode.get(field.toString()), Integer.parseInt(field.getProp("scale")));
+                // for non-logical types
+                default -> avroNode.get(field.toString());
+            };
+
+            deserializedAVRO.put(field.name(), value);
         }
         return deserializedAVRO;
     }
 
+    /**
+     * Set the value to given index position on sql statement based on instance of value.
+     * @param preparedStatement the given prepared statement
+     * @param index the index of binding position
+     * @param value the value of given object
+     * @throws Exception throws when the object type is not support
+     */
     private void setStatement(PreparedStatement preparedStatement, int index, Object value) throws Exception {
         if (value instanceof Integer) {
             preparedStatement.setInt(index, (Integer) value);
@@ -74,8 +85,32 @@ public class OracleAVROSink extends OracleAbstractSink<GenericObject> {
             preparedStatement.setBytes(index, ((ByteString) value).toByteArray());
         } else if (value instanceof Date) {
             preparedStatement.setDate(index, (Date) value);
+        } else if (value instanceof BigDecimal) {
+            preparedStatement.setBigDecimal(index, (BigDecimal) value);
         } else {
             throw new Exception("Not support incoming datatype: " + value.getClass());
         }
+
+    }
+
+    /**
+     * Helper function for convert int value to date.
+     * @param avroDate the integer that contains date.
+     * @return Date value
+     */
+    private Date convertToSQLDate(int avroDate) {
+        LocalDate localDate = LocalDate.ofEpochDay(avroDate);
+        return Date.valueOf(localDate);
+    }
+
+    /**
+     * Helper function for convert array of bytes and scale into big-decimal type.
+     * @param decimalBytes the array of bytes
+     * @param scale the scale of decimal position
+     * @return BigDecimal value
+     */
+    private BigDecimal convertToBigDecimal(byte[] decimalBytes, int scale) {
+        BigInteger unscaledValue = new BigInteger(decimalBytes);
+        return new BigDecimal(unscaledValue, scale);
     }
 }
