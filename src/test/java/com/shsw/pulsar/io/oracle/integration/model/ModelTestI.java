529017a9c16af84a1d9178af8f930eff1c1cbd17
shsw <sae.sahatsawat@gmail.com>
2024-11-12 12:59:38 +0700

    [refactor][test] reduce field for testing primitive data first

--- src/test/java/com/shsw/pulsar/io/oracle/integration/model/ModelTestI.java
+++ src/test/java/com/shsw/pulsar/io/oracle/integration/model/ModelTestI.java
@@ -1,18 +1,10 @@
 package com.shsw.pulsar.io.oracle.integration.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-
-import java.math.BigDecimal;
-import java.util.Date;
 
 
 @Data(staticConstructor = "of")
 public class ModelTestI {
     private int intField;
     private String textField;
-    private BigDecimal bigDecimalField;
-    private Date dateField;
 }
