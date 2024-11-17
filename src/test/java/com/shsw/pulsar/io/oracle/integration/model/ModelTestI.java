2743509bac5f3c0b14c1ac05b26da6beb4a6947b
shsw <sae.sahatsawat@gmail.com>
2024-11-17 12:26:50 +0700

    [refactor][test] add empty constructor and json constructor for json mapper

--- src/test/java/com/shsw/pulsar/io/oracle/integration/model/ModelTestI.java
+++ src/test/java/com/shsw/pulsar/io/oracle/integration/model/ModelTestI.java
@@ -1,11 +1,21 @@
 package com.shsw.pulsar.io.oracle.integration.model;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
-@AllArgsConstructor
 @Data(staticConstructor = "of")
 public class ModelTestI {
     private int intField;
     private String textField;
+
+    public ModelTestI() {
+    }
+
+    @JsonCreator
+    public ModelTestI(@JsonProperty("intField") int intField,
+                      @JsonProperty("textField") String textField) {
+        this.intField = intField;
+        this.textField = textField;
+    }
 }
